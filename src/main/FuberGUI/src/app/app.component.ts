a97b0a840637872b2f0dd461f76f8acbc77e4601
srjn45 <srajanpathak45@gmail.com>
2018-06-03 01:08:57 +0530

    fetced available cabs and active rides from backend sends book, start and end ride requests to backend

--- src/main/FuberGUI/src/app/app.component.ts
+++ src/main/FuberGUI/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FuberService } from './service/fuber.service';
+import { Cab } from './model/cab';
+import { Ride } from './model/ride';
+import { Location } from './model/location';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  public cabs: Cab[] = [];
+  public rides: Ride[] = [];
+
+
+  constructor(private fuberService: FuberService) {
+    this.loadAvailableCabs();
+    this.loadRides();
+  }
+
+  loadAvailableCabs() {
+    this.fuberService.getAvailableCabs().subscribe(res => {
+      if (res.status) {
+        this.cabs = res.payload;
+      }
+    });
+  }
+
+  loadRides() {
+    this.fuberService.getRides().subscribe(res => {
+      if (res.status) {
+        this.rides = res.payload;
+      }
+    });
+  }
+
+  bookRide(form) {
+    console.log(form);
+    let ride = new Ride(null, null, null, form.cabType, new Location(form.latitude, form.longitude), null, null, null);
+    this.fuberService.bookRide(ride).then(res => {
+      if (res.status) {
+        this.loadAvailableCabs();
+        this.loadRides();
+      } else {
+        alert(res.message);
+      }
+    });
+  }
+  startRide(form) {
+    console.log(form);
+    let ride = new Ride(form.otp, null, null, null, null, null, null, null);
+    this.fuberService.startRide(ride).then(res => {
+      if (res.status) {
+        this.loadAvailableCabs();
+        this.loadRides();
+      }
+    });
+  }
+  endRide(form: any, ride: Ride) {
+    console.log(form);
+    ride.distance = form.distance;
+    ride.dropLocation = new Location(form.latitude, form.longitude);
+    this.fuberService.endRide(ride).then(res => {
+      if (res.status) {
+        this.loadAvailableCabs();
+        this.loadRides();
+        alert(res.message);
+      }
+    });
+  }
+
 }
