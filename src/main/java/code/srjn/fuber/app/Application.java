ab8fa531699c7c655d30ea2f4bb704df83b3ba68
srjn45 <srajanpathak45@gmail.com>
2018-06-02 18:23:37 +0530

    added service package to componentScan

--- src/main/java/code/srjn/fuber/app/Application.java
+++ src/main/java/code/srjn/fuber/app/Application.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 
 @SpringBootApplication
-@ComponentScans({ @ComponentScan("code.srjn.fuber.rest") })
+@ComponentScans({ @ComponentScan("code.srjn.fuber.rest"), @ComponentScan("code.srjn.fuber.service") })
 public class Application {
 
 	public static void main(String[] args) {
