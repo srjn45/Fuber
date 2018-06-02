df825b0c11aa2aad101f313e8ed54f6dd123ee5a
srjn45 <srajanpathak45@gmail.com>
2018-06-02 17:38:23 +0530

    cab pojo

---
+++ src/main/java/code/srjn/fuber/domain/Cab.java
@@ -0,0 +1,39 @@
+package code.srjn.fuber.domain;
+
+public class Cab {
+
+	private String licensePlateNumber;
+	private CabType cabType;
+	private Location location;
+
+	public String getLicensePlateNumber() {
+		return licensePlateNumber;
+	}
+
+	public void setLicensePlateNumber(String licensePlateNumber) {
+		this.licensePlateNumber = licensePlateNumber;
+	}
+
+	public CabType getCabType() {
+		return cabType;
+	}
+
+	public void setCabType(CabType cabType) {
+		this.cabType = cabType;
+	}
+
+	public Location getLocation() {
+		return location;
+	}
+
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+
+	@Override
+	public String toString() {
+		return "Cab [licensePlateNumber=" + licensePlateNumber + ", cabType=" + cabType + ", location=" + location
+				+ "]";
+	}
+
+}
