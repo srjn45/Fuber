75dbc2f1390f310286c73c1fc499ac9342a74fa4
srjn45 <srajanpathak45@gmail.com>
2018-06-02 17:38:37 +0530

    location pojo

---
+++ src/main/java/code/srjn/fuber/domain/Location.java
@@ -0,0 +1,34 @@
+package code.srjn.fuber.domain;
+
+public class Location {
+
+	private double latitude;
+	private double longitude;
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public double getDistanceFrom(Location location) {
+		return Math.sqrt(((latitude - location.latitude) * (latitude - location.latitude))
+				+ ((longitude - location.longitude) * (longitude - location.longitude)));
+	}
+
+	@Override
+	public String toString() {
+		return "Location [latitude=" + latitude + ", longitude=" + longitude + "]";
+	}
+
+}
